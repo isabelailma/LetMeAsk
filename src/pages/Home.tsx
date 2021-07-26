8fad641ef21d9ba6451157620f5a00371f3ae16a
Isabela Ilma Gonçalves <isabela.ilma@hotmail.com>
2021-07-26 16:00:57 -0300

    [Mission: ReactJS] Aula 04 - Landing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,6 +39,11 @@ export function Home() {
       return;
     }
 
+    if (roomRef.val().endedAt) {
+      alert('Room already cloded.');
+      return;
+    }
+
     history.push(`/rooms/${roomCode}`);
   }
 
